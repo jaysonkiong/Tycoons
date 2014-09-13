f22862ea7deac3a43fb142abf851505db9a87028
jaysonkiong <jaysonkiong@yahoo.com>
2014-09-13 23:26:05 +0800

    Started the UI

---
+++ Assets/Scripts/SampleScrollList.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleScrollList : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.touchCount > 0) 
+		{
+			Vector2 pos = transform.position;
+			pos.y += Input.GetTouch (0).deltaPosition.x;
+			transform.position = pos;
+		}
+	}
+}
---
+++ Assets/Scripts/TapButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapButton : MonoBehaviour 
+{
+	void OnMouseDown ()
+	{
+		Debug.Log ("I Pressed the " + gameObject.name);
+
+		switch (gameObject.name) 
+		{
+		case "Home":
+			Debug.Log("HOME TO!:");
+			break;
+		case "Market":
+			Debug.Log("MARKET TO!");
+			break;
+		case "Property":
+			Debug.Log("PROPERTY TO!");
+			break;
+		case "Upgrades":
+			Debug.Log("UPGRADES TO!");
+			break;
+		default:
+			Debug.Log("HOME TO");
+			break;
+		}
+	}
+}
